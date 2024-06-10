da2ffa8f1df49a8a8e883a1f4f6855c96106786d
Stephano Tsutsumi <63266761+Stephano-Tsutsumi@users.noreply.github.com>
2024-06-10 15:52:00 -0700

    Update UseCases.tsx

--- components/UseCases.tsx
+++ components/UseCases.tsx
@@ -2,16 +2,18 @@ import React from 'react'
 
 const UseCases = () => {
   return (
-    <div className="pb-20 pt-36 w-full" id="solutions">
+    <div className="pb-20 pt-10 w-full" id="solutions">
         <h1 className="heading">
         <span className="text-purple">Curait in Action</span>
       </h1>
-      <div className='py-10'>
-        <img src="\CuraitCX4.png" alt="" /> 
-      </div>
-      <div className=''>
-        <img src="\CuraitCX3.png" alt="" /> 
-      </div>
+       <div>
+            <div className='py-10'>
+                <img src="\CuraitCX4.png" alt="" /> 
+            </div>
+            <div className=''>
+                <img src="\CuraitCX3.png" alt="" /> 
+            </div>
+        </div>
     </div>
   )
 }
