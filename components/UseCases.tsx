e4663e0fd2a13f628cdf774f984b867afcf19b52
Stephano Tsutsumi <63266761+Stephano-Tsutsumi@users.noreply.github.com>
2024-06-19 15:24:42 -0700

    Update UseCases.tsx

--- components/UseCases.tsx
+++ components/UseCases.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const UseCases = () => {
   return (
-    <div className="py-36 w-full h-screen" id="solutions">
+    <div className="py-36 w-full" id="solutions">
         <h1 className="heading">
         <span className="text-purple">Curait in Action</span>
       </h1>
