1b34b7ff32c0a25f4958d2ff01a1f06a5ae2c15c
Stephano Tsutsumi <63266761+Stephano-Tsutsumi@users.noreply.github.com>
2024-06-19 15:28:22 -0700

    Update UseCases.tsx

--- components/UseCases.tsx
+++ components/UseCases.tsx
@@ -6,7 +6,7 @@ const UseCases = () => {
         <h1 className="heading">
         <span className="text-purple">Curait in Action</span>
       </h1>
-       <div className="w-full h-screen">
+       <div className="w-full h-full">
             <div className='py-10'>
                 <img src="\CuraitCX4.png" alt="" /> 
             </div>
